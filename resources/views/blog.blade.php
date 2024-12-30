45e0ae71d8e2c561749225ce2754f47f19fc57db
M-Naufal-Fadilla <naufalfadilla522@gmail.com>
2024-12-30 21:26:01 +0800

    tugas rakamin

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <a href="about">about</a>
+    <a href="blog">blog</a>
+    <a href="/">home</a>
+    <a href="contact">contact</a>
+    <article>
+        <h1>Blog 1</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis ipsa quam dolorum ducimus neque similique
+            nam enim perferendis repellendus quod consequatur provident quis quos non distinctio, nostrum delectus?
+            Facilis, non!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur doloribus, obcaecati maxime beatae in
+            aspernatur amet, veritatis recusandae adipisci, rem magnam natus totam ex a ad? Repudiandae ducimus tenetur
+            velit?</p>
+    </article>
+    <article>
+        <h1>Blog 2</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis ipsa quam dolorum ducimus neque similique
+            nam enim perferendis repellendus quod consequatur provident quis quos non distinctio, nostrum delectus?
+            Facilis, non!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur doloribus, obcaecati maxime beatae in
+            aspernatur amet, veritatis recusandae adipisci, rem magnam natus totam ex a ad? Repudiandae ducimus tenetur
+            velit?</p>
+    </article>
+        <script src="js/script.js"></script>
+</body>
+
+</html>
